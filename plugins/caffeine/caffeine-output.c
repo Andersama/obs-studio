d6212cb22ee4814fc72f369c76b2737bbbd706e2
Matthew Orlando <cogwheel@caffeine.tv>
2019-04-12 17:20:26 -0700

    caffeine: Remove disabled screenshot code

--- plugins/caffeine/caffeine-output.c
+++ plugins/caffeine/caffeine-output.c
@@ -33,13 +33,6 @@ struct caffeine_output
 	uint64_t start_timestamp;
 	size_t audio_planes;
 	size_t audio_size;
-
-	/*
-	pthread_cond_t screenshot_cond;
-	pthread_mutex_t screenshot_mutex;
-	bool screenshot_needed;
-	AVPacket screenshot;
-	*/
 };
 
 static const char *caffeine_get_name(void *data)
@@ -131,11 +124,6 @@ static void *caffeine_create(obs_data_t *settings, obs_output_t *output)
 		bzalloc(sizeof(struct caffeine_output));
 	context->output = output;
 	context->instance = caff_initialize(caffeine_log, caff_LogLevelInfo);
-
-	/*
-	pthread_mutex_init(&context->screenshot_mutex, NULL);
-	pthread_cond_init(&context->screenshot_cond, NULL);
-	*/
 
 	return context;
 }
@@ -239,15 +227,6 @@ static bool caffeine_start(void *data)
 	if (!obs_output_can_begin_data_capture(context->output, 0))
 		return false;
 
-	/*
-	pthread_mutex_lock(&context->screenshot_mutex);
-	context->screenshot_needed = true;
-	av_init_packet(&context->screenshot);
-	context->screenshot.data = NULL;
-	context->screenshot.size = 0;
-	pthread_mutex_unlock(&context->screenshot_mutex);
-	*/
-
 	obs_service_t *service = obs_output_get_service(context->output);
 	obs_data_t *settings   = obs_service_get_settings(service);
 	char const *title  =
@@ -279,8 +258,6 @@ static void caffeine_stream_started(void *data)
 	obs_output_begin_data_capture(context->output, 0);
 }
 
-static void caffeine_stream_ended(struct caffeine_output *context);
-
 static void caffeine_stream_failed(void *data, caff_Result error)
 {
 	struct caffeine_output *context = data;
@@ -291,8 +268,6 @@ static void caffeine_stream_failed(void *data, caff_Result error)
 			error,
 			caff_resultString(error));
 	}
-
-	caffeine_stream_ended(context);
 
 	obs_output_signal_stop(context->output, caffeine_to_obs_error(error));
 }
@@ -339,136 +314,6 @@ static char const *get_running_game_id(struct caffeine_games *games,
 
 */
 
-/* Called while screenshot_mutex is locked */
-/* Adapted from https://github.com/obsproject/obs-studio/blob/3ddca5863c4d1917ad8443a9ad288f41accf9e39/UI/window-basic-main.cpp#L1741 */
-/* TODO REFACTOR 
-static void create_screenshot(struct caffeine_output *context, uint32_t width,
-		uint32_t height, uint8_t *image_data[MAX_AV_PLANES],
-		uint32_t image_data_linesize[MAX_AV_PLANES],
-		enum video_format format)
-{
-	trace();
-
-	AVCodec           *codec         = NULL;
-	AVCodecContext    *codec_context = NULL;
-	AVFrame           *frame         = NULL;
-	struct SwsContext *sws_context   = NULL;
-	int               got_output     = 0;
-	int               ret            = 0;
-
-	if (image_data == NULL) {
-		log_warn("No image data for screenshot");
-		goto err_no_image_data;
-	}
-
-	// Write JPEG output using libavcodec
-	codec = avcodec_find_encoder(AV_CODEC_ID_MJPEG);
-
-	if (codec == NULL) {
-		log_warn("Unable to load screenshot encoder");
-		goto err_jpeg_codec_not_found;
-	}
-
-	codec_context = avcodec_alloc_context3(codec);
-
-	if (codec_context == NULL) {
-		log_warn("Couldn't allocate codec context");
-		goto err_jpeg_encoder_context_alloc;
-	}
-
-	codec_context->width         = width;
-	codec_context->height        = height;
-	codec_context->pix_fmt       = AV_PIX_FMT_YUVJ422P;
-	codec_context->time_base.num = 1;
-	codec_context->time_base.den = 30;
-	codec_context->bit_rate      = 10000000;
-	codec_context->codec_id      = codec->id;
-	codec_context->codec_type    = AVMEDIA_TYPE_VIDEO;
-
-	if (avcodec_open2(codec_context, codec, NULL) != 0) {
-		log_warn("Couldn't open codec");
-		goto err_jpeg_encoder_open;
-	}
-
-	frame = av_frame_alloc();
-
-	if (frame == NULL) {
-		log_warn("Couldn't allocate frame");
-		goto err_av_frame_alloc;
-	}
-
-	frame->pts    = 1;
-	frame->format = AV_PIX_FMT_YUVJ422P;
-	frame->width  = width;
-	frame->height = height;
-
-	ret = av_image_alloc(frame->data, frame->linesize, codec_context->width, 
-			codec_context->height, codec_context->pix_fmt, 32);
-
-	if (ret < 0) {
-		log_warn("Couldn't allocate image");
-		goto err_av_image_alloc;
-	}
-
-	enum AVPixelFormat src_format = obs_to_ffmpeg_video_format(format);
-
-	// Copy image data, converting RGBA to
-	// image format expected by JPEG encoder
-	sws_context = sws_getContext(frame->width, frame->height, src_format,
-			frame->width, frame->height, codec_context->pix_fmt,
-			0, NULL, NULL, NULL);
-
-	if (sws_context == NULL) {
-		log_warn("Couldn't get scaling context");
-		goto err_sws_getContext;
-	}
-
-	// Transform RGBA to RGB24
-	ret = sws_scale(sws_context, image_data, image_data_linesize, 0,
-			frame->height, frame->data, frame->linesize);
-
-	if (ret < 0) {
-		log_warn("Couldn't translate image format");
-		goto err_sws_scale;
-	}
-
-	av_init_packet(&context->screenshot);
-	context->screenshot.data = NULL;
-	context->screenshot.size = 0;
-
-	ret = avcodec_encode_video2(codec_context, &context->screenshot,
-			frame, &got_output);
-
-	if (ret != 0 || !got_output) {
-		log_warn("Failed to generate screenshot. avcodec_encode_video2 returned %d",
-				ret);
-		goto err_encode;
-	}
-
-err_encode:
-err_sws_scale:
-	sws_freeContext(sws_context);
-	sws_context = NULL;
-err_sws_getContext:
-	av_freep(frame->data);
-	frame->data[0] = NULL;
-err_av_image_alloc:
-	av_frame_free(&frame);
-	frame = NULL;
-err_av_frame_alloc:
-	avcodec_close(codec_context);
-err_jpeg_encoder_open:
-	avcodec_free_context(&codec_context);
-	codec_context = NULL;
-err_jpeg_encoder_context_alloc:
-err_jpeg_codec_not_found:
-err_no_image_data:
-
-	context->screenshot_needed = false;
-	pthread_cond_signal(&context->screenshot_cond);
-}
-*/
-
 static void caffeine_raw_video(void *data, struct video_data *frame)
 {
 #ifdef TRACE_FRAMES
@@ -484,14 +329,6 @@ static void caffeine_raw_video(void *data, struct video_data *frame)
 
 	if (!context->start_timestamp)
 		context->start_timestamp = frame->timestamp;
-
-	/*
-	pthread_mutex_lock(&context->screenshot_mutex);
-	if (context->screenshot_needed)
-		create_screenshot(context, width, height, frame->data,
-			frame->linesize, context->video_info.output_format);
-	pthread_mutex_unlock(&context->screenshot_mutex);
-	*/
 
 	caff_sendVideo(context->instance, frame->data[0], total_bytes,
 		width, height, caff_VideoFormat);
@@ -546,22 +383,6 @@ static void caffeine_raw_audio(void *data, struct audio_data *frames)
 	caff_sendAudio(context->instance, in.data[0], in.frames);
 }
 
-static void caffeine_stream_ended(struct caffeine_output * context)
-{
-	trace();
-	/*
-	pthread_mutex_lock(&context->screenshot_mutex);
-
-	if (context->screenshot.data != NULL) {
-		av_free_packet(&context->screenshot);
-	}
-
-	context->screenshot_needed = false;
-
-	pthread_mutex_unlock(&context->screenshot_mutex);
-	*/
-}
-
 static void caffeine_stop(void *data, uint64_t ts)
 {
 	trace();
@@ -572,7 +393,6 @@ static void caffeine_stop(void *data, uint64_t ts)
 	obs_output_t *output = context->output;
 
 	caff_endBroadcast(context->instance);
-	caffeine_stream_ended(context);
 
 	obs_output_end_data_capture(output);
 }
@@ -581,10 +401,6 @@ static void caffeine_destroy(void *data)
 {
 	trace();
 	struct caffeine_output *context = data;
-	/*
-	pthread_mutex_destroy(&context->screenshot_mutex);
-	pthread_cond_destroy(&context->screenshot_cond);
-	*/
 	caff_deinitialize(&context->instance);
 
 	bfree(data);
